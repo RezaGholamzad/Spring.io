90b9f9970dd9e72ef00a3be64fc1198ed67f80fd
unknown <gholamzadreza@gmail.com>
2020-04-14 20:19:05 +0430

    add securing web application

---
+++ Securing a Web Application/src/main/java/io/spring/securingWebApplication/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package io.spring.securingWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+
+}
---
+++ Securing a Web Application/src/main/java/io/spring/securingWebApplication/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package io.spring.securingWebApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+
+//to enable Spring Security’s web security support and provide the Spring MVC integration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login") // Spring Security provides a filter that intercepts that request and authenticates the user
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    //The userDetailsService() method sets up an in-memory user store with a single user
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withUsername("user")
+                        .password("{noop}password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
